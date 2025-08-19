1c09dd87121e8ad0d24ca549351d1a95e7af6796
Cain J.B. Susko <cain@susko.ca>
2025-08-19 13:14:39 -0300

    Fix lsp keybinds
    for some reason I had them using COC binds rather than generic vim.lsp
    binds so now that is fixed!

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -17,31 +17,29 @@ inoremap '<tab> ''<Esc>i
 inoremap $<tab> $$<Esc>i
 ]])
 
--- Search for file and open in new tab
-map("n", "F<CR>", ":ftab ", opts)
-
 -- Open config in new tab
 vim.api.nvim_create_user_command("Conf", function()
   vim.cmd("tabe ~/.config/nvim/init.lua")
 end, {})
 
--- Coc key mappings (coc.nvim plugin)
-map("n", "\\F", "<Plug>(coc-codeaction-refactor-selected)", {})
-map("n", "\\e", "<Plug>(coc-diagnostic-next)", {})
-map("n", "\\r", "<Plug>(coc-references)", {})
-map("n", "\\R", "<Plug>(coc-rename)", {})
-map("n", "\\t", "<Plug>(coc-type-definition)", {})
-map("n", "\\d", "<Plug>(coc-definition)", {})
-map("n", "\\f", "<Plug>(coc-format)", {})
+-- Mason & built-in LSP key mappings
+-- These assume you are using Mason for LSP server installation + nvim-lspconfig for LSP setup.
+-- Make sure you have your LSP on_attach use these mappings, or set them globally as below.
 
--- Show documentation function
-vim.api.nvim_set_keymap("n", "\\q", ":call ShowDocumentation()<CR>", { noremap = true, silent = true })
-vim.cmd([[
-function! ShowDocumentation()
-  if CocAction('hasProvider', 'hover')
-    call CocActionAsync('definitionHover')
-  else
-    call feedkeys('\q', 'in')
-  endif
-endfunction
-]])
+-- Go to definition
+map("n", "\\d", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- Show references
+map("n", "\\r", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+-- Rename symbol
+map("n", "\\R", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+-- Go to type definition
+map("n", "\\t", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+-- Format document
+map("n", "\\f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+-- Show hover documentation (like ShowDocumentation from coc)
+map("n", "\\q", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+-- Go to next diagnostic
+map("n", "\\e", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+-- [Optional] Code action (refactor, quickfix, etc)
+map("n", "\\F", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+
